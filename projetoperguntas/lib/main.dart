9b0ce57b247b2ae42c43e769ab752f2c8c3025b0
RicMerces <ricardoliveira0703@gmail.com>
2022-03-18 20:01:13 -0300

    Perguntas

---
+++ projetoperguntas/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+          backgroundColor: Colors.purple,
+        ),
+        body: Text('Ola Flutter'),
+      ),
+    );
+  }
+}
