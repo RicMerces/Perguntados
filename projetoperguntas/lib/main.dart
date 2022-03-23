e07a9c0ae5edbf1287aaf701a81544f5b5c64ab8
RicMerces <ricardoliveira0703@gmail.com>
2022-03-23 11:53:33 -0300

    Respondendo Inifinitamente

--- projetoperguntas/lib/main.dart
+++ projetoperguntas/lib/main.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PerguntaApp());
+  runApp(PergutarApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PergutarApp extends StatelessWidget {
+  var novaPergunta = 0;
+
+  int qtdResponder() {
+    novaPergunta++;
+    return novaPergunta;
+  }
+
+  void responder() {
+    print('Pergunta respondida');
+    qtdResponder();
+  }
+
+  final Perguntas = [
+    'Qual o maior time do Nordeste ?',
+    'Qual o time com mais mundiais ?',
+    'Em que time jogou Garrincha ? ',
+  ];
+
+  PergutarApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
           title: Text('Perguntas'),
-          backgroundColor: Colors.purple,
         ),
-        body: Text('Ola Flutter'),
+        body: Column(
+          children: [
+            Text(
+              Perguntas[novaPergunta > (Perguntas.length - 1)
+                  ? novaPergunta = 0
+                  : novaPergunta],
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Botao01'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Botao01'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Botao01'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- projetoperguntas/test/widget_test.dart
+++ projetoperguntas/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:projetoperguntas/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(PerguntaApp());
+    await tester.pumpWidget(PergutarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
